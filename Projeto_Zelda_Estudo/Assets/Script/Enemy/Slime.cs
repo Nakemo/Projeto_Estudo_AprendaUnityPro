07530b65c027256c21be2fb751a15841172d28d9
Nakemo <64189959+Nakemo@users.noreply.github.com>
2023-03-22 19:46:14 -0300

    State Machine Part.1

--- Projeto_Zelda_Estudo/Assets/Script/Enemy/Slime.cs
+++ Projeto_Zelda_Estudo/Assets/Script/Enemy/Slime.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Slime : MonoBehaviour
@@ -7,11 +8,22 @@ public class Slime : MonoBehaviour
     private Animator anim;
     public int HP;
 
+    public enemyState state;
+
+    public const float idleWaitTime = 3f;
+    public const float patrolWaitTime = 5f;
+
     private bool isDead;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+        ChangeState(state);
+    }
+
+    private void Update() 
+    {
+        StateManager();
     }
 
 
@@ -40,6 +52,80 @@ public class Slime : MonoBehaviour
             StartCoroutine("Died");
         }
     }
+
+    #region State Machine
+    private void StateManager() 
+    {
+        switch (state) 
+        {
+            case enemyState.IDLE:
+                break;
+
+            case enemyState.ALERT:
+                break;
+
+            case enemyState.EXPLORE:
+                break;
+
+            case enemyState.FOLLOW:
+                break;
+
+            case enemyState.FURY:
+                break;
+
+            case enemyState.PATROL:
+                break;
+        }
+    }
+
+    private void ChangeState(enemyState newState) 
+    {
+        print(newState); //Check on console
+        StopAllCoroutines();       
+        state = newState;
+
+        switch (state)
+        {
+            case enemyState.IDLE:                
+                StartCoroutine("IDLE");
+                break;
+
+            case enemyState.ALERT:
+
+                break;
+
+            case enemyState.PATROL:
+                StartCoroutine("PATROL");
+                break;
+        }
+    }
+
+    #region IEnumarators
+    IEnumerator IDLE() 
+    {
+        yield return new WaitForSeconds(idleWaitTime);
+        if (Rand() < 50)
+        {
+            ChangeState(enemyState.IDLE);
+        }
+        else 
+        {
+            ChangeState(enemyState.PATROL);
+        }
+    }
+    IEnumerator PATROL() 
+    {
+        yield return new WaitForSeconds(patrolWaitTime);
+        ChangeState(enemyState.IDLE);
+    }
+    #endregion
+
+    private int Rand() 
+    {
+        int rand = Random.Range(0, 100);
+        return rand;
+    }
+    #endregion
 
     #endregion
 }
--- Projeto_Zelda_Estudo/Assets/Script/System/GameManager.cs
+++ Projeto_Zelda_Estudo/Assets/Script/System/GameManager.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum enemyState 
+{
+    IDLE, ALERT, PATROL, EXPLORE, FOLLOW, FURY
+}
+
+
+
 public class GameManager : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
